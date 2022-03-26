9a04aa30bf262f28e8591ddaa20f0ecc90a5ba17
owsei <owseiwastaken@gmail.com>
2022-03-26 16:43:33 -0300

    better timer gb && --test mode

--- timer.go
+++ timer.go
@@ -6,20 +6,21 @@ type timer struct {
 	time float64
 	label string
 	index int
-	ln int
+	done bool
 }
 
 var (
 	timers = []timer{}
 )
 
-func (t timer) run (i float64) ( bool ) { // time passed -> running
+func run (t timer, i float64) ( bool ) { // time passed -> running
 	if i < t.time {
 		printf("%s%s: %.1f seconds left \n", spos(t.index, 0), t.label, t.time-i)
 		return true
 	} else {
 		print(spos(t.index, 0) + "\x1b[K")
 		printf("%s%s%s: completed!%s\n", spos(t.index, 0), RGB(0, 255, 0), t.label, RGB(255,255,255))
+		t.done = true
 		return false
 	}
 }
@@ -27,6 +28,12 @@ func (t timer) run (i float64) ( bool ) { // time passed -> running
 func main(){
 	InitGu()
 	clear()
+	test := get("--test").Exists
+	if test {
+		argv = []string{"4.3:timer", "de", "quatro.3", "segs", "5", ".5:iês"}
+		argc = len(argv)
+		printf("%stesting %s\n", spos(4, 0), argv)
+	}
 
 	var a string // argv[i]
 	var ts []string // temporary string
@@ -66,9 +73,10 @@ func main(){
 			// get time
 			time, err = strconv.ParseFloat(a, 64)
 			panic(err)
-			timers = append(timers, timer{time, label, len(timers),
-				len(spf("%s: %.1f seconds left \n", label, time)),
-			})
+
+			// make timer
+			timers = append(timers, timer{time, label, len(timers), false})
+
 		} else if (i != 0) {
 			timers[len(timers)-1].label += " " + a
 		}
@@ -82,7 +90,14 @@ func main(){
 		sleep(0.1)
 		for i:=0;i<len(timers);i++{
 			t = timers[i]
-			r = t.run(j) || r
+			r = run(t, j) || r
+			if t.done {
+				timers = append(timers[:i], timers[i+1:]...)
+				//r = true
+				i--
+			}
 		}
 	}
+	pos(len(timers), 0)
+	exit(0)
 }
