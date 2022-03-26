2f970f4ef8e05043b22488daa45fa7e36263812a
owsei <owseiwastaken@gmail.com>
2022-03-26 10:23:46 -0300

    better timer!

--- timer.go
+++ timer.go
@@ -2,58 +2,88 @@ package main
 
 include "gutil"
 
-import (
-	"sort"
+type timer struct {
+	time float64
+	label string
+	index int
+	ln int
+}
+
+var (
+	timers = []timer{}
 )
 
-func _conv ( x string ) (int, error) {
-	a, e := strconv.Atoi(x[:len(x)-1])
-	switch (x[len(x)-1]) {
-		case 's', 'S'://seconds
-			return a, e
-		case 'm', 'M'://seconds
-			return a*60, e
-		case 'h', 'H'://seconds
-			return a*60*60, e
+func (t timer) run (i float64) ( bool ) { // time passed -> running
+	if i < t.time {
+		printf("%s%s: %.1f seconds left \n", spos(t.index, 0), t.label, t.time-i)
+		return true
+	} else {
+		print(spos(t.index, 0) + "\x1b[K")
+		printf("%s%s%s: completed!%s\n", spos(t.index, 0), RGB(0, 255, 0), t.label, RGB(255,255,255))
+		return false
 	}
-	return 0, e
-}
-
-func conv (x string) (int) {
-	r, e := _conv(x)// result, error
-	panic(e)
-	return r
-}
-
-func tmr ( slp int, index int) () {
-	time.Sleep( time.Duration(1000000.0*slp) )
-	x:=len(sprintf("%d: %d seconds left\n", index, slp))
-	for i:=0;i<slp;i++{
-		printf("%s%d: %d seconds left \n", spos(index, 0), index, slp-i)
-		sleep(1)
-	}
-	printf("%s%s \n", spos(index, 0), strings.Repeat(" ", x))
-	printf("%s%s%d: completed!%s\n", spos(index, 0), RGB(0, 255, 0), index, RGB(255,255,255))
 }
 
 func main(){
-	if argc == 0 {
-		exit(0)
-	}
 	InitGu()
 	clear()
 
-	HideCursor()
-	var os = make([]int, argc)
-	for i:=0;i<argc;i++ {
-		os[i] = conv(argv[i])
-	}
-	sort.Ints(os)
-	for i:=0;i<argc-1;i++ {
-		go tmr(os[i], i)
-	}
-	tmr(os[argc-1], argc-1)
-	ShowCursor()
+	var a string // argv[i]
+	var ts []string // temporary string
+	var err error
 
-	exit(0)
+	var ( // make timer vars
+		time float64
+		label string
+		// index = i
+	)
+	if argc == 0 {
+		printf("%s[timer error]%s: timer.go needs at least one argument\n", RGB(255, 0, 0), RGB(255, 255, 255))
+		exit(1)
+	}
+	// get args & make timers
+	for i:=0;i<argc;i++{
+		a = argv[i]
+
+		if (a[0] == 46) {
+			a = "0"+a
+		}
+
+		if ((47 < a[0] && a[0] < 58)) { // 0..9
+			label = spf("timer %d", i) // default label
+
+			// has a label
+			if strings.Contains(a, ":") {
+				ts = strings.Split(a, ":")
+				if len(ts) == 2{
+					a = ts[0]
+					label = ts[1]
+				} else {
+					panic(errors.New(spf("$%d has more than one ':'", i+1)))
+				}
+			}
+
+			// get time
+			time, err = strconv.ParseFloat(a, 64)
+			panic(err)
+			timers = append(timers, timer{time, label, len(timers),
+				len(spf("%s: %.1f seconds left \n", label, time)),
+			})
+		} else if (i != 0) {
+			timers[len(timers)-1].label += " " + a
+		}
+	}
+
+	var t timer// timers[i]
+	var r = true
+
+	for j:=0.0;r;j+=0.1{
+		r = false
+		sleep(0.1)
+		for i:=0;i<len(timers);i++{
+			t = timers[i]
+			r = t.run(j) || r
+		}
+	}
+
 }
