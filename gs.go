0b43cc4496d9cfd6076b0752a4f372b7b4400167
owseiwastaken <owseiwastaken@gmail.com>
2022-06-11 10:49:07 -0300

    cleaned gs.go

--- gs.go
+++ gs.go
@@ -30,7 +30,7 @@ var (
 		"→",
 		"↑",
 		"↓",
-		"🖉",// i monospace goes brrr
+		"🖉",
 	}
 	flags = make([]bool, FlagLen)
 )
@@ -39,7 +39,6 @@ func main () {
 	InitGu()
 	var (
 		GSOut string
-	//	GD bool // git directory
   )
 
 	Out, err := exec.Command("git", "status").Output()
@@ -55,27 +54,24 @@ func main () {
 		// PS(strings.Contains(GSOut, FlagTexts[i]))
 		// print("\n")
 		flags[i] = strings.Contains(GSOut, FlagTexts[i])
-		//GD = GD || flags[i]
 	}
 
-	GSOut = ""
+	GSOut = "("
+	if flags[0] {
+		GSOut += RGB(60, 255, 60)
+	} else if flags[3] {
+		GSOut += RGB(60, 60, 255)
+	} else {
+		GSOut += RGB(255, 60, 60)
+	}
 
-	//if GD {
-		GSOut += "("
-		if flags[0] {
-			GSOut += RGB(60, 255, 60)
-		} else if flags[3] {
-			GSOut += RGB(60, 60, 255)
-		} else {
-			GSOut += RGB(255, 60, 60)
+	for i:=0;i<FlagLen;i++ {
+		if flags[i] {
+			GSOut += FlagIcons[i]
 		}
-		for i:=0;i<FlagLen;i++ {
-			if flags[i] {
-				GSOut += FlagIcons[i]
-			}
-		}
-		GSOut += RGB(255, 255, 255) + ")"
-	//}
+	}
+
+	GSOut += RGB(255, 255, 255) + ")"
 	GSOut = strings.Replace(GSOut, " ", "", -1)
 	printf(GSOut)
 
