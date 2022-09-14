dddd0a562e5e11c0235f13ad8868e627d7dc6493
owseiwastaken <owseiwastaken@gmail.com>
2022-09-14 10:00:22 -0300

    removed comments, removed clear func, added T/: interactions

--- color.go
+++ color.go
@@ -17,14 +17,12 @@ func main(){
 
 	var (
 		CharBuff []byte
-		//red = RGB(255, 0, 0)
-		//green = RGB(0, 255, 0)
-		//blue = RGB(0, 0, 255)
-		//IdToColor = map[int]string{
-		//	0:red,
-		//	1:green,
-		//	2:blue,
-		//}
+		ClearChar = "\033[2K"
+		e error
+		HadError bool
+		ri int
+		gi int
+		bi int
 		IdToColorS = map[int]string{
 			0:"red",
 			1:"green",
@@ -40,43 +38,35 @@ func main(){
 		}
 		IncCol = func() () { cla[ColorId] = ( cla[ColorId]+1 ) }
 		DecCol = func() () { cla[ColorId] = (cla[ColorId]+255) }
+		SIncCol = func() () { cla[ColorId] = ( cla[ColorId]+10 ) }
+		SDecCol = func() () { cla[ColorId] = (cla[ColorId]+246) }
 		oz []int
+		TestText = "Lorem Ipsum"
 	)
 
 	InitGu()
 	InitGetCh()
 	clear()
 	var (
-		up	  = []byte{27, 91, 65, 0, 0}
-		down  = []byte{27, 91, 66, 0, 0}
-		right = []byte{27, 91, 67, 0, 0}
-		left  = []byte{27, 91, 68, 0, 0}
-		enter = []byte{10, 0, 0, 0, 0}
+		up	  = []byte{27, 91, 65}
+		down  = []byte{27, 91, 66}
+		right = []byte{27, 91, 67}
+		left  = []byte{27, 91, 68}
+		enter = []byte{10}
+		dcol  = []byte{58}
+		t     = []byte{116}
+		sup   = []byte{27, 91, 49, 59, 50, 65}
+		sdown = []byte{27, 91, 49, 59, 50, 66}
+		//TODO cup/cdown = all++
+		//TODO scup/scdown = all+10
 	)
 
-	//pos(0, 0)
-	//printf("%s\x1b[48;2;255;255;255m%s\x1b[48;2;0;0;0m%s:%d ",
-	//	IATC(oz), IdToColorS[i], COLOR["nc"], cla[i],
-	//)
-	//for i:=0;i<3;i++{
-	//	if (i == ColorId) {
-	//		printf("%s%s%s:%d ",
-	//			IdToColor[i], IdToColorS[i], COLOR["nc"], cla[i],
-	//		)
-	//	} else {
-	//		printf("%s:%d ",
-	//			IdToColorS[i], cla[i],
-	//		)
-	//	}
-	//}
-	//printf("\n%s█████ █████\nLorem Ipsum%s\n",
-	//	RGB(cla[0], cla[1], cla[2]), COLOR["nc"],
-	//)
 	HideCursor()
 	oz = []int{0, 0, 0}
 	for {
 		pos(0, 0)
 
+		print(ClearChar)
 		for i:=0;i<3;i++{
 			oz[i] = int(cla[i])
 			if ( i == ColorId ) {
@@ -91,20 +81,65 @@ func main(){
 			oz[i] = 0
 		}
 
-		printf("\n%s█████ █████\nLorem Ipsum%s\n",
-			RGB(cla[0], cla[1], cla[2]), COLOR["nc"],
+		printf("\n%s%s█████ █████\n%s%s%s\n",
+			ClearChar, RGB(cla[0], cla[1], cla[2]), ClearChar, TestText, COLOR["nc"],
 		)
+		stdout.Flush()
 		CharBuff = make([]byte, 5)
-		CharBuff = GetChByte()
-		clear()
+		CharBuff = GetChByte(stdin)
+
+		pos(8, 0)
+		printf("%s%v", ClearChar, CharBuff)
+
+		if (CompareBytes(sup, CharBuff)) {SIncCol()} else
+		if (CompareBytes(sdown, CharBuff)) {SDecCol()} else
 		if (CompareBytes(up, CharBuff)) {IncCol()} else
 		if (CompareBytes(down, CharBuff)) {DecCol()} else
 		if (CompareBytes(right, CharBuff)) {IncId()} else
 		if (CompareBytes(left, CharBuff)) {DecId()} else
 		if (CompareBytes(enter, CharBuff)) {break}
+		if (CompareBytes(dcol, CharBuff)) {
+			pos(2, 0)
+			ShowCursor()
+			print("\nR:")
+			r:=oldinput()
+			print("\nG:")
+			g:=oldinput()
+			print("\nB:")
+			b:=oldinput()
+
+			ri, e = strconv.Atoi(r)
+			HadError = (e!=nil)
+			gi, e = strconv.Atoi(g)
+			HadError = HadError||(e!=nil)
+			bi, e = strconv.Atoi(b)
+			HadError = HadError||(e!=nil)
+			if !HadError {
+				if ((ri < 255) && (gi < 255) && (bi < 255)) {
+					cla[0] = uint8(ri)
+					cla[1] = uint8(gi)
+					cla[2] = uint8(bi)
+				}
+			}
+			pos(2, 0)
+			print("\n"+ClearChar)
+			print("\n"+ClearChar)
+			print("\n"+ClearChar)
+
+			HideCursor()
+		}
+		if (CompareBytes(t, CharBuff)) {
+			pos(2, 0)
+			print(ClearChar+RGB(cla[0], cla[1], cla[2]))
+			ShowCursor()
+			TestText = oldinput()
+			print(COLOR["nc"])
+			HideCursor()
+		}
 	}
+	pos(0,0)
 	ShowCursor()
-	printf("gcall: RGB(%d, %d, %d)\n", cla[0], cla[1], cla[2])
-	printf("ansii: \"\\x1b[38;2;%d;%d;%dm\"\n", cla[0], cla[1], cla[2])
+	printf("%sgcall: RGB(%d, %d, %d)\n", ClearChar, cla[0], cla[1], cla[2])
+	printf("%sansii: \"\\x1b[38;2;%d;%d;%dm\"\n", ClearChar, cla[0], cla[1], cla[2])
 	exit(0)
 }
