35d4228bc4b5bbf668a462f2ed3212aaf69a0c8a
owsei <owseiwastaken@gmail.com>
2022-09-29 23:13:58 -0300

    parse .txt, find by xy, better display

--- elements.go
+++ elements.go
@@ -3,132 +3,22 @@ package main
 include "gutil"
 
 type El struct {
-	symbol string
-	name string
-	nome string
+	Z int
+	x int
+	y int
+	fam int
 	mass float64
-	Family int
+	srs []string
 }
 
-var (
-	elements = []El{
-		//0
-		El{"ERROR", "ERROR", "ERROR", 0, 0},
-		//1
-		El{"H" , "Hydrogen", "Hidrogenio", 1.0079, 1},
-		El{"He", "Helium", "Helio", 4.0026, 8},
-		El{"Li", "Lithium", "Litio", 6.9410, 1},
-		El{"Be", "Beryllium", "Berilio", 9.0122, 2},
-		El{"B" , "Boron", "Boro", 10.8110, 3},
-		El{"C" , "Carbon", "Carbono", 12.0107, 4},
-		El{"N" , "Nitrogen", "Nitrogenio", 14.0067, 5},
-		El{"O" , "Oxygen", "Oxigenio", 15.9994, 6},
-		El{"F" , "Fluorine", "Fluor", 18.9984, 7},
-		El{"Ne", "Neon", "Neon", 20.1797, 8},
-		El{"Na", "Sodium", "Sodio", 22.9897, 1},
-		El{"Mg", "Magnesium", "Magnesio", 24.3050, 2},
-		El{"Al", "Aluminum", "Aluminio", 26.9815, 3},
-		El{"Si", "Silicon", "Silicio", 28.0855, 4},
-		El{"P" , "Phosphorus", "Fosforo", 30.9738, 5},
-		El{"S" , "Sulfur", "Enxofre", 32.0650, 6},
-		El{"Cl", "Chlorine", "Cloro", 35.4530, 7},
-		El{"Ar", "Argon", "Argon", 39.9480, 2},
-		El{"K" , "Potassium", "Potassio", 39.0983, 1},
-		//20
-		El{"Ca", "Calcium", "Calcio", 40.0780, 2},
-		El{"Sc", "Scandium", "Escandio", 44.9559, 11},
-		El{"Ti", "Titanium", "Titanio", 47.8670, 12},
-		El{"V" , "Vanadium", "Vanadio", 50.9415, 13},
-		El{"Cr", "Chromium", "Cromo", 51.9961, 14},
-		El{"Mn", "Manganese", "Manganes", 54.9380, 15},
-		El{"Fe", "Iron", "Ferro", 55.8450, 15},
-		El{"Ni", "Nickel", "Cobalto", 58.6934, 16},
-		El{"Co", "Cobalt", "Niquel", 58.9332, 16},
-		El{"Cu", "Copper", "Cobre", 63.5460, 9},
-		El{"Zn", "Zinc", "Zinco", 65.3900, 10},
-		El{"Ga", "Gallium", "Galio", 69.7230, 3},
-		El{"Ge", "Germanium", "Germanio", 72.6400, 4},
-		El{"As", "Arsenic", "Arsenio", 74.9216, 5},
-		El{"Se", "Selenium", "Selenio", 78.9600, 6},
-		El{"Br", "Bromine", "Bromo", 79.9040, 7},
-		El{"Kr", "Krypton", "Cripton", 83.8000, 8},
-		El{"Rb", "Rubidium", "Rubidio", 85.4678, 1},
-		El{"Sr", "Strontium", "Estroncio", 87.6200, 2},
-		El{"Y" , "Yttrium", "Itrio", 88.9059, 11},
-		//40
-		El{"Zr", "Zirconium", "Zirconio", 91.2240, 12},
-		El{"Nb", "Niobium", "Niobio", 92.9064, 13},
-		El{"Mo", "Molybdenum", "Molibdenio", 95.9400, 14},
-		El{"Tc", "Technetium", "Tecnecio", 98.0000, 15},
-		El{"Ru", "Ruthenium", "Rutenio", 101.0700, 15},
-		El{"Rh", "Rhodium", "Rodio", 102.9055, 16},
-		El{"Pd", "Palladium", "Paladio", 106.4200, 16},
-		El{"Ag", "Silver", "Prata", 107.8682, 9},
-		El{"Cd", "Cadmium", "Cadmio", 112.4110, 10},
-		El{"In", "Indium", "Índio", 114.8180, 3},
-		El{"Sn", "Tin", "Estanho", 118.7100, 4},
-		El{"Sb", "Antimony", "Antimonio", 121.7600, 5},
-		El{"Te", "Tellurium", "Telurio", 127.6000, 6},
-		El{"I" , "Iodine", "Iodo", 126.9045, 7},
-		El{"Xe", "Xenon", "Xenonio", 131.2930, 8},
-		El{"Cs", "Cesium", "Cesio", 132.9055, 1},
-		El{"Ba", "Barium", "Bario", 137.3270, 2},
-		El{"La", "Lanthanum", "Lantanio", 138.9055, 11},
-		El{"Ce", "Cerium", "Cerio", 140.1160, 11},
-		El{"Pr", "Praseodymium", "Praseodimio", 140.9077, 11},
-		//60
-		El{"Nd", "Neodymium", "Neodimio", 144.2400, 11},
-		El{"Pm", "Promethium", "Promecio", 145.0000, 11},
-		El{"Sm", "Samarium", "Samario", 150.3600, 11},
-		El{"Eu", "Europium", "Europio", 151.9640, 11},
-		El{"Gd", "Gadolinium", "Gadolinio", 157.2500, 11},
-		El{"Tb", "Terbium", "Terbio", 158.9253, 11},
-		El{"Dy", "Dysprosium", "Disprosio", 162.5000, 11},
-		El{"Ho", "Holmium", "Holmio", 164.9303, 11},
-		El{"Er", "Erbium", "Érbio", 167.2590, 11},
-		El{"Tm", "Thulium", "Tulio", 168.9342, 11},
-		El{"Yb", "Ytterbium", "Iterbio", 173.0400, 11},
-		El{"Lu", "Lutetium", "Lutecio", 174.9670, 11},
-		El{"Hf", "Hafnium", "Hafnio", 178.4900, 12},
-		El{"Ta", "Tantalum", "Tantalo", 180.9479, 13},
-		El{"W" , "Tungsten", "Tungstenio", 183.8400, 14},
-		El{"Re", "Rhenium", "Renio", 186.2070, 15},
-		El{"Os", "Osmium", "Ósmio", 190.2300, 15},
-		El{"Ir", "Iridium", "Iridio", 192.2170, 16},
-		El{"Pt", "Platinum", "Platina", 195.0780, 16},
-		El{"Au", "Gold", "Ouro", 196.9665, 9},
-		//80
-		El{"Hg", "Mercury", "Mercurio", 200.5900, 10},
-		El{"Tl", "Thallium", "Talio", 204.3833, 3},
-		El{"Pb", "Lead", "Chumbo", 207.2000, 4},
-		El{"Bi", "Bismuth", "Bismuto", 208.9804, 5},
-		El{"Po", "Polonium", "Polonio", 209.0000, 6},
-		El{"At", "Astatine", "Astato", 210.0000, 7},
-		El{"Rn", "Radon", "Radonio", 222.0000, 8},
-		El{"Fr", "Francium", "Francio", 223.0000, 1},
-		El{"Ra", "Radium", "Radio", 226.0000, 2},
-		El{"Ac", "Actinium", "Actinio", 227.0000, 11},
-		El{"Th", "Thorium", "Torio", 232.0381, 11},
-		El{"Pa", "Protactinium", "Protactinio", 231.0359, 11},
-		El{"U" , "Uranium", "Uranio", 238.0289, 11},
-		El{"Np", "Neptunium", "Neptunio", 237.0000, 11},
-		El{"Pu", "Plutonium", "Plutonio", 244.0000, 11},
-		El{"Am", "Americium", "Americio", 243.0000, 11},
-		El{"Cm", "Curium", "Curio", 247.0000, 11},
-		El{"Bk", "Berkelium", "Berquelio", 247.0000, 11},
-		El{"Cf", "Californium", "Californio", 251.0000, 11},
-		El{"Es", "Einsteinium", "Einstenio", 252.0000, 11},
-		//100
-		El{"Fm", "Fermium", "Fermio", 257.0000, 11},
-		El{"Md", "Mendelevium", "Mendelevio", 258.0000, 11},
-		El{"No", "Nobelium", "Nobelio", 259.0000, 11},
-		El{"Lr", "Lawrencium", "Laurencio", 262.0000, 11},
-	} //1..103
-	NameToE = map[string]El{}
-	NomeToE = map[string]El{}
-	SymbolToE = map[string]El{}
-	mlist = []float64{}
-)
+func GetByCoors(x, y int) (El) {
+	for i:=0;i<len(elements);i++ {
+		if elements[i].x == x && elements[i].y == y {
+			return elements[i]
+		}
+	}
+	return elements[0]
+}
 
 func FindClosetsMass (target float64) (El) {
 	var (
@@ -136,11 +26,11 @@ func FindClosetsMass (target float64) (El) {
 		d = 0.0
 		//last diff
 		index int
-		md = target-mlist[0]
+		md = target-elements[0].mass
 	)
 
-	for i:=0;i<len(mlist);i++ {
-		d = target-mlist[i]
+	for i:=0;i<len(elements);i++ {
+		d = target-elements[i].mass
 		if d < 0 { d*=-1 }
 		if d < md {
 			md = d
@@ -166,7 +56,7 @@ func GetFam(ins string) ([]El) {
 	panic(err)
 	var ret = []El{}
 	for i:=1;i<len(elements);i++ {
-		if elements[i].Family == in {
+		if elements[i].fam == in {
 			ret = append(ret, elements[i])
 		}
 	}
@@ -196,15 +86,19 @@ func MeasureDiff (a, b string) (int) {
 
 func FindClosetsName (target string) (El) {
 	le := elements[0]
+	// started as 'biggest'
 	lowest := len(target)*missingdiff+wrongdiff
-	t := 0
-	t2 := 0
+	t := 999
+	t2:= 999
 
 	for i:=1;i<len(elements);i++ {
-		t = MeasureDiff(elements[i].name, target)
-		t2 = MeasureDiff(elements[i].nome, target)
-		if t2<t {
-			t = t2
+		for j:=0;j<len(elements[i].srs);j++ {
+			t = MeasureDiff(elements[i].srs[j], target)
+			if t2<t {
+				t = t2
+			} else {
+				t2 = t
+			}
 		}
 		if lowest > t {
 			lowest = t
@@ -215,26 +109,83 @@ func FindClosetsName (target string) (El) {
 	return le
 }
 
+func (e El) str() (string) {
+	// assume srs[0] is the symbol
+	return spf("%d:%s", e.Z, e.srs[0])
+}
+
+func (e El) display() (string) {
+	ns := ""
+	for i:=1;i<len(e.srs);i++ {
+		ns+=", "+e.srs[i]
+	}
+	return spf(
+		"%s (%d,%d) Z=%d M=%f \"%s\"",
+		e.srs[0], e.x, e.y, e.Z, e.mass, ns[2:],
+	)
+}
+
+var elements = []El{}
 func main(){
 	InitGu()
+
+	fl:=strings.Split(ReadFile("Els.txt"), "\n")
 	var E El
-	for i:=1;i<len(elements);i++ {
-		E = elements[i]
-		NameToE[E.name] = E
-		NomeToE[E.nome] = E
-		SymbolToE[E.symbol] = E
-		mlist = append(mlist, E.mass)
+	var tint int
+	var index = 0
+	var f float64
+	var terror error
+	var line []string
+
+	for i:=0;i<len(fl);i++{
+		line = strings.Split(fl[i], ",")
+		if len(line) < 5 {continue}
+		if fl[i][0] == '#' {continue}
+
+		E = El{index, 0, 0, 0, 0.0, []string{}}
+
+		tint, terror = strconv.Atoi(line[0])
+		panic(terror)
+		E.x = tint
+		tint, terror = strconv.Atoi(line[1])
+		panic(terror)
+		E.y = tint
+		tint, terror = strconv.Atoi(line[2])
+		panic(terror)
+		E.fam = tint
+		f, terror = strconv.ParseFloat(line[3], 32)
+		panic(terror)
+		E.mass = f
+
+		for i:=4;i<len(line);i++ {
+			E.srs = append(E.srs, line[i])
+		}
+
+		elements = append(elements, E)
+		index++
 	}
+
 	E = elements[0]
 	var a string
+	var ts []string
 	var t int
+	var t2 int
 	var e error
+	var scope []El
 
 	for i:=0;i<argc;i++{
 		a = argv[i]
 		if (a[0] == 'a' || a[0] == 'A' || a[0] == 'b' || a[0] == 'B') {
 			//TODO: define E
-			PS(GetFam(a))
+			scope = GetFam(a)
+			PS(scope)
+		} else if strings.Index(a, ",")!=-1 {
+			ts = strings.Split(a, ",")
+			t, e = strconv.Atoi(ts[0])
+			panic(e)
+			t2, e = strconv.Atoi(ts[1])
+			panic(e)
+			E = GetByCoors(t, t2)
 		} else if a[len(a)-1] == 'm' {
 			t, e = strconv.Atoi(a[:len(a)-1])
 			panic(e)
@@ -248,7 +199,7 @@ func main(){
 				E = FindClosetsName(a)
 			}
 		}
-		PS(E)
+		PS(E.display())
 	}
 
 
