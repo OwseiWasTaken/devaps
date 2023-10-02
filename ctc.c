8feb0ff8dd508e94193b6af74cb779c77be05c83
Manse <pedro.manse@dmk3.com.br>
2023-10-02 18:36:27 +0000

    -1 flag for ctc, to simplify piping

--- ctc.c
+++ ctc.c
@@ -3,29 +3,51 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int piped = 0;
 void char_to_code(char *text) {
 	for (int i = 0; i<strlen(text); i++) {
-		printf("%c:%d\n", text[i], (int)text[i]);
+		if (piped) {
+			printf("%d\n", (int)text[i]);
+		} else {
+			printf("%c:%d\n", text[i], (int)text[i]);
+		}
 	}
 }
 
 void code_to_char(int code) {
+	if (piped) {
+		printf("%c", (char)code);
+		return;
+	}
+
 	if (isprint(code)) {
 		printf("%d:%c\n", code, (char)code);
 	} else {
-		printf("%d:\\x%02x\n", code, code);
+		printf("%d:0x%02x\n", code, code);
 	}
 }
 
 int main(int argc, char **argv) {
+	if (argc <= 1) return EXIT_SUCCESS;
+
+	if (!strncmp(argv[1], "-1", 2)) {
+		argc--; // skip flag
+		argv = &(argv[1]);
+		piped = 1;
+	}
+
 	// -1 flag to print
 	for (int i = 1; i<argc; i++) {
 		if (isdigit(argv[i][0])) {
-			// split with strtok(":-, ")
-			code_to_char(atoi(argv[i]));
+			char *digit = strtok(argv[i], ":-, ");
+			while (digit) {
+				code_to_char(atoi(digit));
+				digit = strtok(NULL, ":-, ");
+			}
 		} else {
 			char_to_code(argv[i]);
 		}
 	}
+	return EXIT_SUCCESS;
 }
 
