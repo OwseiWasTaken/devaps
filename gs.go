5659ec6b590d775eb3b13e6f86cfee4367121845
Pedro Manse <pedro.manse@dmk3.com.br>
2023-10-26 15:29:02 -0300

    gc.py version of gs is not legacy, gs.go is now smaller and simpler

--- gs.go
+++ gs.go
@@ -1,88 +1,52 @@
 package main
 
-include "gutil"
-
-// flag index
-const (
-	Clean = iota
-	Utracked = iota
-	uncommited = iota
-	unpushed = iota
-	unpulled = iota
-	restore = iota
-	FlagLen = iota
+import (
+	"fmt"
+	"strings"
+	"os/exec"
 )
 
+type Status struct {
+	Text string
+	Icon rune
+	Color string
+}
 
-// flag texts
+func (s Status) Format() string {
+	return fmt.Sprintf("\x1b[%sm%c", s.Color, s.Icon)
+}
+
+func (s Status) Match(GSText string) bool {
+	return strings.Contains(GSText, s.Text)
+}
+
 var (
-	FlagTexts = []string{
-		"working tree clean",
-		"Untracked files",
-		"Changes to be committed",
-		"Your branch is ahead",
-		"behind",
-		"Changes not staged for commit",
+	// could use map[string]rune
+	// but would have random order of icons
+	StatusList = []Status{
+		{"working tree clean"           , 'ü', "0;32"},
+		{"Untracked files"              , '+', "0;31"},
+		{"Changes to be committed"      , '→', "0;31"},
+		{"Your branch is ahead"         , '↑', "0;34"},
+		{"behind"                       , '↓', "1;91"},
+		{"Changes not staged for commit", '*', "0;31"},
 	}
-	FlagIcons = []string{
-		"ü",
-		"+",
-		"→",
-		"↑",
-		"↓",
-		"*",
-	}
-	flags = make([]bool, FlagLen)
 )
 
-func main () {
-	InitGu()
-	var (
-		GSOut string
-		branch string
-  )
-
+func main() {
 	Out, err := exec.Command("git", "status").Output()
+	if (err != nil) {panic(err)}
+	GSText := string(Out)
 
-	// not git directory
-	if err != nil{
-		exit(0)
-	}
+	branchline := strings.Split(GSText, "\n")[0]
+	branchname := strings.TrimPrefix(branchline, "On branch ")
 
-	GSOut = string(Out)
-	branch = strings.Split(GSOut, "\n")[0]
-	branch = strings.Join(strings.Split(branch, " ")[2:], " ")
-
-	for i:=0;i<FlagLen;i++ {
-		flags[i] = strings.Contains(GSOut, FlagTexts[i])
-	}
-
-
-	// always print branch
-	GSOut = RGB(255, 60, 180)+branch+" "
-	// print branch is not default
-	//if branch != "master" && branch != "main" {
-	//	GSOut = "("+branch+" "
-	//} else {
-	//	GSOut = "("
-	//}
-
-	if flags[0] {
-		GSOut += RGB(60, 255, 60)
-	} else if flags[3] {
-		GSOut += RGB(60, 60, 255)
-	} else {
-		GSOut += RGB(255, 60, 60)
-	}
-
-	for i:=0;i<FlagLen;i++ {
-		if flags[i] {
-			GSOut += FlagIcons[i]
+	var icons string
+	for _, status := range StatusList {
+		if status.Match(GSText) {
+			icons+=status.Format()
 		}
 	}
 
-	GSOut += RGB(255, 255, 255)
-	printf(GSOut)
-
-	exit(0)
+	fmt.Printf("\x1b[0;35m%s %s\x1b[0m", branchname, icons)
 }
---
+++ legacy/gs.go
@@ -0,0 +1,89 @@
+package main
+
+include "gutil"
+
+// flag index
+const (
+	clean = iota
+	utracked = iota
+	uncommited = iota
+	unpushed = iota
+	unpulled = iota
+	restore = iota
+	FlagLen = iota
+)
+
+
+// flag texts
+var (
+	FlagTexts = []string{
+		"working tree clean",
+		"Untracked files",
+		"Changes to be committed",
+		"Your branch is ahead",
+		"behind",
+		"Changes not staged for commit",
+	}
+	FlagIcons = []string{
+		"ü",
+		"+",
+		"→",
+		"↑",
+		"↓",
+		"*",
+	}
+	//TODO: make binary int flag
+	flags = make([]bool, FlagLen)
+)
+
+func main () {
+	InitGu()
+	var (
+		GSOut string
+		branch string
+  )
+
+	Out, err := exec.Command("git", "status").Output()
+
+	// not git directory
+	if err != nil{
+		exit(0)
+	}
+
+	GSOut = string(Out)
+	branch = strings.Split(GSOut, "\n")[0]
+	branch = strings.Join(strings.Split(branch, " ")[2:], " ")
+
+	for i:=0;i<FlagLen;i++ {
+		flags[i] = strings.Contains(GSOut, FlagTexts[i])
+	}
+
+
+	// always print branch
+	GSOut = RGB(255, 60, 180)+branch+" "
+	// print branch is not default
+	//if branch != "master" && branch != "main" {
+	//	GSOut = "("+branch+" "
+	//} else {
+	//	GSOut = "("
+	//}
+
+	if flags[0] {
+		GSOut += RGB(60, 255, 60)
+	} else if flags[3] {
+		GSOut += RGB(60, 60, 255)
+	} else {
+		GSOut += RGB(255, 60, 60)
+	}
+
+	for i:=0;i<FlagLen;i++ {
+		if flags[i] {
+			GSOut += FlagIcons[i]
+		}
+	}
+
+	GSOut += RGB(255, 255, 255)
+	printf(GSOut)
+
+	exit(0)
+}
