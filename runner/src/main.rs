3a267178d74e5efb4809ddb28fe43d29efd4125a
Pedro Manse <pedro.manse@dmk3.com.br>
2024-03-02 00:16:19 -0300

    finished .cfg parser, making verify methok, still must verify if one of findAny exists

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -5,7 +5,9 @@ struct Config {
 	has: Vec<String>,
 	find_any: Vec<String>,
 	exec: Vec<String>,
+	env: Vec<String>,
 }
+//TODO implement hasAny
 
 impl Config {
 	fn new(name: String) -> Self {
@@ -14,23 +16,79 @@ impl Config {
 			has: Vec::new(),
 			find_any: Vec::new(),
 			exec: Vec::new(),
+			env: Vec::new(),
 		}
 	}
 
-	fn append_has(&mut self, has: String) { self.has.push(has) }
-	fn append_find(&mut self, find: String) { self.find_any.push(find) }
-	fn append_exec(&mut self, exec: String) { self.exec.push(exec) }
+	fn get_setting(&mut self, name: &str) -> Result<&mut Vec<String>, String> {
+		Ok(match name {
+			"has"=>&mut self.has,
+			"findAny"=>&mut self.find_any,
+			"exec"=>&mut self.exec,
+			"env"=>&mut self.env,
+			_=>{return Err(format!("Unknown name {name}"))}
+		})
+	}
+	fn append<T>(&mut self, name: &str, values: T) -> Result<(), String>
+		where T: Iterator<Item = String>
+	{
+		let var = self.get_setting(name)?;
+		values.for_each(|value|var.push(value));
+		Ok(())
+	}
+
+	fn verify(&self) -> Result<bool, String> {
+		let has_files = self.has
+			.iter()
+			.map(std::path::Path::new)
+			.map(std::path::Path::try_exists);
+		let errors: Vec<_> = has_files
+			.clone()
+			.filter(Result::is_err)
+			.map(Result::unwrap_err)
+			.collect();
+		if let Some(error) = errors.first() {
+			return Err(error.to_string());
+		}
+		let has_files = has_files
+			.map(Result::unwrap) // safe unwrap, no errors in iterator
+			.fold(true, |every, current|every&&current);
+
+		//TODO find one of find_any
+		Ok(has_files)
+	}
+
+	fn execute(self) -> () {
+		todo!()
+	}
 }
 
 fn read_config(content: &str) -> Result<Config, String> {
-	todo!("read_config")
+	let mut lines = content.split("\n");
+	let scope = lines.next().ok_or("Can't read scope setter")?;
+	let scope = scope[..scope.len()-1].to_owned();
+
+	let mut config = Config::new(scope);
+	for line in lines {
+		if line.is_empty() {continue}
+		let (name, values) = line.split_once("=").ok_or("non-empty line doens't have '=' to set a variable")?;
+		let values = values.split(":").map(str::to_owned);
+		config.append(name, values)?;
+	}
+
+	Ok(config)
 }
 
 fn read_configs(content: &str) -> Result<Vec<Config>, String> {
-	let scopes = content.split("\n[");
-	scopes.map(read_configs)
-	read_configs
-	todo!("read_configs")
+	let mut configs = Vec::new();
+	for scope in content.split("\n[") {
+		if !scope.is_empty() {
+			// first line may be '[' without '\n'
+			let scope = scope.strip_prefix("[").unwrap_or(scope);
+			configs.push(read_config(scope)?);
+		}
+	}
+	Ok(configs)
 }
 
 fn main() -> Result<(), String> {
@@ -43,7 +101,10 @@ fn main() -> Result<(), String> {
 
 	let configfile = format!("{home}/.config/runner.cfg");
 	let content = std::fs::read_to_string(configfile).expect("can't read file");
-	let config = read_configs(&content)?;
+	let configs = read_configs(&content)?;
+	for config in configs {
+		println!("{}: {}", &config.name, config.verify()?);
+	}
 
 	// TODO: parse agrs
 	// --help
@@ -51,7 +112,6 @@ fn main() -> Result<(), String> {
 	// --explain
 	// --inspect
 	//let args = std::env::args();
-	println!("{config:?}");
 
 	Ok(())
 }
