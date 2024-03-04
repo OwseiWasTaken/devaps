7616d4c95a11bf582ec269787db54249e11f2b8e
Pedro Manse <pedro.manse@dmk3.com.br>
2024-03-04 07:55:10 -0300

    idk

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -2,7 +2,7 @@
 #[derive(Debug)]
 struct Config {
 	name: String,
-	has: Vec<String>,
+	has: Vec<Vec<String>>,
 	find_any: Vec<String>,
 	exec: Vec<String>,
 	env: Vec<String>,
@@ -22,40 +22,52 @@ impl Config {
 
 	fn get_setting(&mut self, name: &str) -> Result<&mut Vec<String>, String> {
 		Ok(match name {
-			"has"=>&mut self.has,
 			"findAny"=>&mut self.find_any,
 			"exec"=>&mut self.exec,
 			"env"=>&mut self.env,
 			_=>{return Err(format!("Unknown name {name}"))}
 		})
 	}
-	fn append<T>(&mut self, name: &str, values: T) -> Result<(), String>
-		where T: Iterator<Item = String>
+	fn append(&mut self, name: &str, values: Vec<String>) -> Result<(), String>
 	{
-		let var = self.get_setting(name)?;
-		values.for_each(|value|var.push(value));
+		if name == "has" {
+			self.has.push(values);
+		} else {
+			let var = self.get_setting(name)?;
+			values.into_iter().for_each(|value|var.push(value));
+		}
 		Ok(())
 	}
 
-	fn verify(&self) -> Result<bool, String> {
-		let has_files = self.has
-			.iter()
-			.map(std::path::Path::new)
-			.map(std::path::Path::try_exists);
-		let errors: Vec<_> = has_files
-			.clone()
-			.filter(Result::is_err)
-			.map(Result::unwrap_err)
-			.collect();
-		if let Some(error) = errors.first() {
-			return Err(error.to_string());
-		}
-		let has_files = has_files
-			.map(Result::unwrap) // safe unwrap, no errors in iterator
-			.fold(true, |every, current|every&&current);
+	fn verify_verbose(&self) -> Result<bool, String> {
+		todo!()
+	}
 
-		//TODO find one of find_any
-		Ok(has_files)
+	fn verify(&self, verbose: bool) -> Result<bool, String> {
+		//TODO print _after_ check
+		//println!("{}{{", &self.name);
+		for files in &self.has {
+			'found: {
+				for file in files {
+					let exists  = std::path::Path::new(&file)
+						.try_exists()
+						.map_err(|e|e.to_string())?;
+					if exists {
+						if verbose {
+							//println!("\t\x1b[32m✓ {}\x1b[0m", &file);
+						}
+						break 'found
+					}
+				}
+				if verbose {
+					//println!("\t\x1b[31mX {}\x1b[0m", files.join(" : "));
+				}
+				return Ok(false)
+			}
+		}
+
+		//println!("}}\x1b[32m✓ {}\x1b[0m", &self.name);
+		Ok(true)
 	}
 
 	fn execute(self) -> () {
@@ -63,57 +75,112 @@ impl Config {
 	}
 }
 
-fn read_config(content: &str) -> Result<Config, String> {
-	let mut lines = content.split("\n");
-	let scope = lines.next().ok_or("Can't read scope setter")?;
-	let scope = scope[..scope.len()-1].to_owned();
+impl std::str::FromStr for Config {
+	type Err = String;
 
-	let mut config = Config::new(scope);
-	for line in lines {
-		if line.is_empty() {continue}
-		let (name, values) = line.split_once("=").ok_or("non-empty line doens't have '=' to set a variable")?;
-		let values = values.split(":").map(str::to_owned);
-		config.append(name, values)?;
+	fn from_str(content: &str) -> Result<Config, String> {
+		let mut lines = content.split("\n");
+		let scope = lines.next().ok_or("Can't read scope setter")?;
+		let scope = scope[..scope.len()-1].to_owned();
+
+		let mut config = Config::new(scope);
+		for line in lines {
+			if line.is_empty() {continue}
+			let (name, values) = line.split_once("=").ok_or("non-empty line doens't have '=' to set a variable")?;
+			let values = values.split(":").map(str::to_owned);
+			config.append(name, values.collect::<Vec<_>>())?;
+		}
+
+		Ok(config)
 	}
-
-	Ok(config)
 }
 
 fn read_configs(content: &str) -> Result<Vec<Config>, String> {
-	let mut configs = Vec::new();
-	for scope in content.split("\n[") {
-		if !scope.is_empty() {
-			// first line may be '[' without '\n'
-			let scope = scope.strip_prefix("[").unwrap_or(scope);
-			configs.push(read_config(scope)?);
-		}
-	}
-	Ok(configs)
+	content.split("\n[")
+		.filter(|e|!e.is_empty())
+		.map(|scope|scope.strip_prefix("[").unwrap_or(scope))
+		.map(|scope|scope.parse())
+		.fold(Ok(Vec::new()), |may_vec: Result<Vec<Config>, String>, may_cfg|{
+			match may_cfg {
+				Ok(cfg)=>may_vec.map(|mut vec|{vec.push(cfg);vec}),
+				Err(e)=>Err(e)
+			}
+		})
+}
+
+fn get_home_dir() -> Result<String, String> {
+	std::env::vars()
+		.filter(|(name, _value)| name == "HOME")
+		.next()
+		.map(|(_name, value)| value)
+		.ok_or("$HOME not defined!".to_owned())
 }
 
 fn main() -> Result<(), String> {
-	let vars = std::env::vars();
-	let home = vars
-		.filter(|(name, _)| name == "HOME")
-		.next()
-		.map(|(_, value)| value)
-		.ok_or("$HOME not defined!".to_owned())?;
 
-	let configfile = format!("{home}/.config/runner.cfg");
-	let content = std::fs::read_to_string(configfile).expect("can't read file");
-	let configs = read_configs(&content)?;
-	for config in configs {
-		println!("{}: {}", &config.name, config.verify()?);
+	let mut configfile: Option<String> = None;
+	let mut verify = false;
+	let mut verbose = false;
+	let mut help = false;
+
+	let mut args = std::env::args();
+	let proc_name = args.next();
+	while let Some(arg) = args.next() {
+		match arg.as_str() {
+			"-h"|"--help"=>help = true,
+			"-v"|"--verify"=>verify = true,
+			"-V"|"-vv"|"--verbose_verify"=>verbose = true,
+			"-c"|"--config"=>{
+				//TODO
+				configfile = Some(args.next().ok_or("--config must provide config file")?);
+			}
+			"-"=>{},
+			_=>{
+				println!("{}", display_help(proc_name));
+				return Err(format!("Unknown argument '{}'", arg));
+			},
+		};
 	}
 
-	// TODO: parse agrs
-	// --help
-	// --config
-	// --explain
-	// --inspect
-	//let args = std::env::args();
+	if help {
+		println!("{}", display_help(proc_name));
+		return Ok(())
+	}
+
+	let configfile = match configfile {
+		None=>{
+			let home = get_home_dir()?;
+			format!("{home}/.config/runner.cfg")
+		},
+		Some(file)=>file,
+	};
+	let content = std::fs::read_to_string(&configfile).expect("can't read file");
+	let configs = read_configs(&content)?;
+
+	let scope = verify_all(configs, verbose)?
+		.ok_or(format!("Can't find single run scope from {configfile}"))?;
+	println!("{scope:?}");
 
 	Ok(())
+}
+
+fn verify_all(configs: Vec<Config>, verbose: bool) -> Result<Option<Config>, String> {
+	for config in configs {
+		if config.verify(verbose)? {
+			return Ok(Some(config))
+		}
+	}
+	Ok(None)
+}
+
+fn display_help(progname: Option<String>) -> String {
+	format!( "usage $ {}\n\
+		\t[-v | --verify] prints what scope \x1b[3mwould\x1b[0m run\n\
+		\t[-V | -vv | --verbose_verify] prints what scope would run, \x1b[3mand why\x1b[0m\n\
+		\t[-h | --help] prints this\n\
+		\t[-c \x1b[3mconfigfile\x1b[0m | --config \x1b[3mconfigfile\x1b[0m] use \x1b[3mconfigfile\x1b[0m instead of $HOME/.config.runner.cfg",
+		progname.unwrap_or("run".to_owned())
+	)
 }
 
 // TODO execute shell commands with $file as env var for first file found from the findAny vec
