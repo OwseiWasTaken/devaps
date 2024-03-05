887b3677d7e8e44a3228de4b0caae14581e830ab
Pedro Manse <pedro.manse@dmk3.com.br>
2024-03-05 13:15:22 -0300

    finally implemented executing commands with runner.rs; still need to make better verify and verbose verify, and change 'findAny' to 'find'

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,13 +1,11 @@
-#[allow(dead_code)]
 #[derive(Debug)]
 struct Config {
 	name: String,
 	has: Vec<Vec<String>>,
 	find_any: Vec<String>,
 	exec: Vec<String>,
-	env: Vec<String>,
+	env: Vec<(String, String)>,
 }
-//TODO implement hasAny
 
 impl Config {
 	fn new(name: String) -> Self {
@@ -24,12 +22,19 @@ impl Config {
 		Ok(match name {
 			"findAny"=>&mut self.find_any,
 			"exec"=>&mut self.exec,
-			"env"=>&mut self.env,
 			_=>{return Err(format!("Unknown name {name}"))}
 		})
 	}
-	fn append(&mut self, name: &str, values: Vec<String>) -> Result<(), String>
-	{
+	fn append(&mut self, name: &str, values: &str) -> Result<(), String> {
+		if name == "env" {
+			let (env_name, env_val) = values
+				.split_once(" ")
+				.ok_or(format!("env `{values}` definition must contain space to split name and value"))?;
+			self.env.push((env_name.to_owned(), env_val.to_owned()));
+			return Ok(());
+		}
+
+		let values: Vec<String> = values.split(":").map(str::to_owned).collect();
 		if name == "has" {
 			self.has.push(values);
 		} else {
@@ -39,10 +44,15 @@ impl Config {
 		Ok(())
 	}
 
-	fn verify_verbose(&self) -> Result<bool, String> {
-		todo!()
+	fn find(&self) -> Result<Option<String>, String> {
+		self.find_any.iter().try_any_which(|file|{
+			std::path::Path::new(file)
+				.try_exists()
+				.map_err(|e|e.to_string())
+		}).map(|opt_str|opt_str.map(|st|st.clone()))
 	}
 
+	//TODO verify even if findAny doens't work
 	fn verify(&self, verbose: bool) -> Result<bool, String> {
 		//TODO print _after_ check
 		//println!("{}{{", &self.name);
@@ -65,13 +75,45 @@ impl Config {
 				return Ok(false)
 			}
 		}
+		if !self.find_any.is_empty() {
+			self.find()?.ok_or("Can't find single findAny file")?;
+		}
 
 		//println!("}}\x1b[32m✓ {}\x1b[0m", &self.name);
 		Ok(true)
 	}
 
-	fn execute(self) -> () {
-		todo!()
+	// TODO print command beeing executed
+	fn execute(self) -> Result<(), String> {
+		use std::process::Command;
+
+		let file = if self.find_any.is_empty() {
+			None
+		} else {
+			Some(self.find()?.ok_or("Can't find single findAny file")?)
+		};
+
+		for cmd in self.exec.into_iter() {
+			let mut child = Command::new("/bin/sh");
+			child.arg("-c");
+			child.arg(&cmd);
+			if let Some(ref found) = file {
+				child.env("found", found);
+			}
+			for (name, value) in self.env.iter() {
+				child.env(name, value);
+			}
+			child.spawn()
+				.map_err(|e|{
+					format!("can't spawn {}: {}", cmd, e.to_string())
+				})?
+				.wait()
+				.map_err(|e|{
+					format!("can't execute {}: {}", cmd, e.to_string())
+				})?;
+		}
+
+		Ok(())
 	}
 }
 
@@ -87,8 +129,7 @@ impl std::str::FromStr for Config {
 		for line in lines {
 			if line.is_empty() {continue}
 			let (name, values) = line.split_once("=").ok_or("non-empty line doens't have '=' to set a variable")?;
-			let values = values.split(":").map(str::to_owned);
-			config.append(name, values.collect::<Vec<_>>())?;
+			config.append(name, values)?;
 		}
 
 		Ok(config)
@@ -157,20 +198,17 @@ fn main() -> Result<(), String> {
 	let content = std::fs::read_to_string(&configfile).expect("can't read file");
 	let configs = read_configs(&content)?;
 
-	let scope = verify_all(configs, verbose)?
+	let scope = configs.into_iter()
+		.try_any_which(|config|config.verify(verbose))?
 		.ok_or(format!("Can't find single run scope from {configfile}"))?;
-	println!("{scope:?}");
+
+	if verify || verbose {
+		println!("Found scope {}", scope.name);
+	} else {
+		scope.execute()?;
+	}
 
 	Ok(())
-}
-
-fn verify_all(configs: Vec<Config>, verbose: bool) -> Result<Option<Config>, String> {
-	for config in configs {
-		if config.verify(verbose)? {
-			return Ok(Some(config))
-		}
-	}
-	Ok(None)
 }
 
 fn display_help(progname: Option<String>) -> String {
@@ -183,14 +221,100 @@ fn display_help(progname: Option<String>) -> String {
 	)
 }
 
-// TODO execute shell commands with $file as env var for first file found from the findAny vec
-//		let child = std::process::Command::new("/bin/sh")
-//			.env("found", self.Found)
-//			.arg("-c")
-//			.arg(&self.command)
-//			// inherit from parent or pipe?
-//			//.stdin(Stdio::piped())
-//			//.stdout(Stdio::piped())
-//			//.stderr(Stdio::piped())
-//			.spawn()
-//			.expect("Can't execute command");
+trait All: IntoIterator {
+
+	// cool methods for iterators
+	//fn all<F>(self, fnc: F) -> bool
+	//where
+	//	Self: Sized,
+	//	F: Fn(Self::Item) -> bool,
+	//{
+	//	for item in self.into_iter() {
+	//		if !fnc(item) {return false}
+	//	}
+	//	true
+	//}
+
+	//fn all_where<F>(self, fnc: F) -> Option<usize>
+	//where
+	//	Self: Sized,
+	//	F: Fn(Self::Item) -> bool,
+	//{
+	//	let mut counter = 0;
+	//	for item in self.into_iter() {
+	//		if !fnc(item) {
+	//			return Some(counter)
+	//		}
+	//		counter+=1;
+	//	}
+	//	None
+	//}
+
+	//fn all_which<F>(self, fnc: F) -> Option<Self::Item>
+	//where
+	//	Self: Sized,
+	//	F: Fn(&Self::Item) -> bool,
+	//{
+	//	for item in self.into_iter() {
+	//		if !fnc(&item) {
+	//			return Some(item)
+	//		}
+	//	}
+	//	None
+	//}
+
+	//fn any<F>(self, fnc: F) -> bool
+	//where
+	//	Self: Sized,
+	//	F: Fn(Self::Item) -> bool,
+	//{
+	//	for item in self.into_iter() {
+	//		if fnc(item) {return true}
+	//	}
+	//	false
+	//}
+
+	//fn any_where<F>(self, fnc: F) -> Option<usize>
+	//where
+	//	Self: Sized,
+	//	F: Fn(Self::Item) -> bool,
+	//{
+	//	let mut counter = 0;
+	//	for item in self.into_iter() {
+	//		if fnc(item) {
+	//			return Some(counter)
+	//		}
+	//		counter+=1;
+	//	}
+	//	None
+	//}
+
+	//fn any_which<F>(self, fnc: F) -> Option<Self::Item>
+	//where
+	//	Self: Sized,
+	//	F: Fn(&Self::Item) -> bool,
+	//{
+	//	for item in self.into_iter() {
+	//		if fnc(&item) {
+	//			return Some(item)
+	//		}
+	//	}
+	//	None
+	//}
+
+	fn try_any_which<F, E>(self, fnc: F) -> Result<Option<Self::Item>, E>
+	where
+		Self: Sized,
+		F: Fn(&Self::Item) -> Result<bool, E>,
+	{
+		for item in self.into_iter() {
+			if fnc(&item)? {
+				return Ok(Some(item))
+			}
+		}
+		Ok(None)
+	}
+
+}
+
+impl<I: Iterator> All for I {}
